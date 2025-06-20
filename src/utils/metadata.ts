e8a05b3b0c41f0e98900bab6b9b4a849c612b6e7
Marcus Smith <marcushsmith@comcast.net>
2025-06-19 22:04:22 -0700

    improve og image

--- src/utils/metadata.ts
+++ src/utils/metadata.ts
@@ -3,7 +3,7 @@ import { Metadata } from "next";
 export const siteConfig = {
   name: "Linear Top Issue",
   description: "Find the most important Linear issue for your team",
-  url: "https://linear-top-issue.app",
+  url: "https://www.linear-top-issue.app",
   ogImage: "/images/linear_top_issue_og_image.jpg",
 } as const;
 
@@ -30,6 +30,7 @@ export const metadataConfig: Metadata = {
         width: 1200,
         height: 630,
         alt: siteConfig.name,
+        type: "image/jpeg",
       },
     ],
   },
@@ -37,8 +38,16 @@ export const metadataConfig: Metadata = {
     card: "summary_large_image",
     title: siteConfig.name,
     description: siteConfig.description,
-    images: [siteConfig.ogImage],
+    images: [
+      {
+        url: siteConfig.ogImage,
+        width: 1200,
+        height: 630,
+        alt: siteConfig.name,
+      },
+    ],
     creator: "@marcusmth",
+    site: "@marcusmth",
   },
   robots: {
     index: true,
